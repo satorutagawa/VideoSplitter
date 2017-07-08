a01779f5222ccb99fd6e86b74b249dac41e3853a
tagawa <tagawa@fixstars.com>
2017-07-07 23:32:09 -0700

    Fixed octal error in convert2sec

--- run_split_video.sh
+++ run_split_video.sh
@@ -1,3 +1,4 @@
 #!/usr/local/bin/bash
 
+#./split_video.sh input.mp4 test_split_times.txt
 ./split_video.sh input.mp4 split_times.txt
--- split_video.sh
+++ split_video.sh
@@ -4,7 +4,7 @@ set -e
 
 function convert2sec () {
 	IFS=":"; read -a hhmmss <<< "$1";
-	sec=$((60*60*${hhmmss[0]}+60*${hhmmss[1]}+${hhmmss[2]}))
+	sec=$((60*60*10#${hhmmss[0]}+60*10#${hhmmss[1]}+10#${hhmmss[2]}))
 	echo ${sec}
 }
 
@@ -17,10 +17,20 @@ ctr=1
 while read line
 do
 	OUTFILE="${ctr}.${ext}"
-	echo "${line}"
+
+#	echo "${line}"
 	IFS=' '; read -a fields <<< "$line";
+
+	# Convert 0:10:03 to 603 seconds
 	start_sec=$(convert2sec ${fields[0]})
 	end_sec=$(convert2sec ${fields[1]})
+
+	# split file
 	ffmpeg -y -i ${1} -ss ${start_sec} -to ${end_sec} ${OUTFILE} < /dev/null 2> /dev/null
+
+	# Using sem to run multiple instances of ffmpeg. Not any faster
+	#sem -j+0 ffmpeg -y -i ${1} -ss ${start_sec} -to ${end_sec} ${OUTFILE} < /dev/null 2> /dev/null ";" echo done
+
 	((ctr++))
 done < "$2"
+sem --wait
